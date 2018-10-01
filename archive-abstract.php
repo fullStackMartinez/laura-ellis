44582be7580c72b5cb06632bb88ac20ff7722b33
Esteban Martinez <fullstackmartinez@gmail.com>
2018-10-01 01:38:30 -0600

    archive landscape loop reformatted code

--- archive-abstract.php
+++ archive-abstract.php
@@ -5,57 +5,55 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-	<header class="page-header">
-		<?php
-		the_archive_title( '<h1 class="page-title">', '</h1>' );
-		the_archive_description( '<div class="archive-description">', '</div>' );
-		?>
-	</header><!-- .page-header -->
+			<header class="page-header">
+				<?php
+				the_archive_title('<h1 class="page-title">', '</h1>');
+				the_archive_description('<div class="archive-description">', '</div>');
+				?>
+			</header><!-- .page-header -->
 
-		<!-- START OF ABSTRACT ARTWORK LOOP -->
-<?php
-	$args = array(
-	'posts_per_page'	=> -1,
-	'post_type'		=> 'Artwork',
-	'category_name' => 'Abstract',
-	);
+			<!-- START OF ABSTRACT ARTWORK LOOP -->
+			<?php
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'Artwork',
+				'category_name' => 'Abstract',
+			);
 
-	// query
-	$the_query = new WP_Query( $args );
+			// query
+			$the_query = new WP_Query($args);
 
-	?>
-<?php if( $the_query->have_posts() ): ?>
-	<?php while ( $the_query->have_posts() ) : $the_query->the_post();?>
-	<?php $featured = get_field('is_this_a_sold_item?'); ?>
-	<div class="artwork-container"><?php
-		the_post_thumbnail(); ?>
-		<p><?php the_title(); ?></p>
+			?>
+			<?php if($the_query->have_posts()): ?>
+				<?php while($the_query->have_posts()) : $the_query->the_post(); ?>
+					<?php $featured = get_field('is_this_a_sold_item?'); ?>
+					<div class="artwork-container"><?php
+						the_post_thumbnail(); ?>
+						<p><?php the_title(); ?></p>
 
-		<?php the_field('artwork_year'); ?>,
-		<?php the_field('artwork_medium'); ?>,
-		<?php the_field('width'); ?>,
-		<?php the_field('height'); ?>
+						<?php the_field('artwork_year'); ?>,
+						<?php the_field('artwork_medium'); ?>,
+						<?php the_field('width'); ?>,
+						<?php the_field('height'); ?>
 
-		<!-- IF THIS CUSTOM FIELD HAS A SPECIFIC VALUE, ECHO SOMETHING OUT -->
-		<?php if( isset($featured) && $featured == "y" ) : ?>
-			Private Collection
-		<?php endif; ?>
+						<!-- IF THIS CUSTOM FIELD HAS A SPECIFIC VALUE, ECHO SOMETHING OUT -->
+						<?php if(isset($featured) && $featured == "y") : ?>
+							Private Collection
+						<?php endif; ?>
 
-		</div>
-<?php
+					</div>
+				<?php
 
- endwhile; endif;?>
+				endwhile; endif; ?>
 
-		<!-- END OF ABSTRACT LOOP -->
-
-
+			<!-- END OF ABSTRACT LOOP -->
 
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_footer();
--- archive-landscape.php
+++ archive-landscape.php
@@ -5,30 +5,54 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-	<header class="page-header">
-		<?php
-		the_archive_title( '<h1 class="page-title">', '</h1>' );
-		the_archive_description( '<div class="archive-description">', '</div>' );
-		?>
-	</header><!-- .page-header -->
+			<header class="page-header">
+				<?php
+				the_archive_title('<h1 class="page-title">', '</h1>');
+				the_archive_description('<div class="archive-description">', '</div>');
+				?>
+			</header><!-- .page-header -->
 
-		<?php while ( have_posts() ) : the_post(); ?>
+			<!-- START OF ABSTRACT ARTWORK LOOP -->
+			<?php
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'Artwork',
+				'category_name' => 'Landscape',
+			);
 
-			<h1><?php the_field('custom_title'); ?></h1>
+			// query
+			$the_query = new WP_Query($args);
 
-			<img src="<?php the_field('hero_image'); ?>" />
+			?>
+			<?php if($the_query->have_posts()): ?>
+				<?php while($the_query->have_posts()) : $the_query->the_post(); ?>
+					<?php $featured = get_field('is_this_a_sold_item?'); ?>
+					<div class="artwork-container"><?php
+						the_post_thumbnail(); ?>
+						<p><?php the_title(); ?></p>
 
-			<p><?php the_content(); ?></p>
+						<?php the_field('artwork_year'); ?>,
+						<?php the_field('artwork_medium'); ?>,
+						<?php the_field('width'); ?>,
+						<?php the_field('height'); ?>
 
-		<?php endwhile; // end of the loop. ?>
+						<!-- IF THIS CUSTOM FIELD HAS A SPECIFIC VALUE, ECHO SOMETHING OUT -->
+						<?php if(isset($featured) && $featured == "y") : ?>
+							Private Collection
+						<?php endif; ?>
 
+					</div>
+				<?php
 
+				endwhile; endif; ?>
 
-	</main><!-- #main -->
-</div><!-- #primary -->
+			<!-- END OF ABSTRACT LOOP -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_footer();
