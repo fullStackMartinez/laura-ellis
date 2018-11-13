4bafcbe4240ff3a8f5e064751f4314755cb6473f
Esteban Martinez <fullstackmartinez@gmail.com>
2018-11-12 20:54:42 -0700

    restyled single artwork page

--- single.php
+++ single.php
@@ -13,49 +13,43 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<!-- Next previous loop that collects post ids of a category and uses them for navigation next/previous -->
+			<?php
+			$post_id = $post->ID; // current post id
+			$cat = get_the_category();
+			$current_cat_id = $cat[0]->cat_ID; // current category Id
+
+			$args = array(
+				'post_type' => 'artwork',
+				'category' => $current_cat_id,
+				'order' => 'DESC',
+				'posts_per_page' => -1
+			);
+			$posts = get_posts($args);
+			// get ids of posts retrieved from get_posts
+			$ids = array();
+			foreach($posts as $thepost) {
+				$ids[] = $thepost->ID;
+			}
+			// get and echo previous and next post in the same category
+			$thisindex = array_search($post->ID, $ids);
+			$previd = $ids[$thisindex - 1];
+			$nextid = $ids[$thisindex + 1];
+			?>
 			<div class="upfront">
-				<?php
-				$post_id = $post->ID; // current post id
-				$cat = get_the_category();
-				$current_cat_id = $cat[0]->cat_ID; // current category Id
-
-				$args = array(
-					'post_type' => 'artwork',
-					'category' => $current_cat_id,
-					'order' => 'DESC',
-					'posts_per_page' => -1
-				);
-				$posts = get_posts($args);
-				// get ids of posts retrieved from get_posts
-				$ids = array();
-				foreach($posts as $thepost) {
-					$ids[] = $thepost->ID;
-				}
-				// get and echo previous and next post in the same category
-				$thisindex = array_search($post->ID, $ids);
-				$previd = $ids[$thisindex - 1];
-				$nextid = $ids[$thisindex + 1];
-
-				?>
-
-				<?php
-				while(have_posts()) :
-				the_post(); ?>
 				<div class="close">
 					<a href="<?php echo get_category_link($current_cat_id); ?>"> X </a>
 				</div>
 				<?php
 				if(!empty($previd)) {
 					?>
-					<a rel="prev" href="<?php echo get_permalink($previd) ?>" class="prev">< </a>
+					<a rel="prev" href="<?php echo get_permalink($previd) ?>" class="prev"> < </a>
 					<?php
 				}
 				the_post_thumbnail(); ?>
 				<?php
 				if(!empty($nextid)) {
 					?>
-					<a rel="next" href="<?php echo get_permalink($nextid) ?>" class="next">></a>
+					<a rel="next" href="<?php echo get_permalink($nextid) ?>" class="next"> > </a>
 					<?php
 				} ?>
 			</div>
@@ -75,10 +69,10 @@ get_header();
 			</div>
 			<?php
 
-			endwhile; // End of the loop.
 
 			wp_reset_postdata();
 			?>
+			<!-- Next previous loop that collects post ids of a category and uses them for navigation next/previous -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
